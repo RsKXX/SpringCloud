7761a8e743f3a1a6723d2730f06f95f8ac29e227
RuanShaoKang <1584853990@qq.com>
2021-11-18 09:49:05 +0800

    添加依赖，添加Nacos配置

---
+++ auth/src/main/java/com/study/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.study.controller;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController("/test")
+//public class TestController {
+//
+//    @GetMapping("/getReq")
+//    public String getRequest(){
+//        return "get请求成功";
+//    }
+//}
