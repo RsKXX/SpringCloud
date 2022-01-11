9d38a39c69533c0e614fd0122d0df63c6077ad03
RuanShaoKang <1584853990@qq.com>
2022-01-11 10:42:34 +0800

    修改shiro 查询和其他配置

--- shiro/src/main/java/com/shiro/mapper/RoleMapper.java
+++ shiro/src/main/java/com/shiro/mapper/RoleMapper.java
@@ -2,11 +2,9 @@ package com.shiro.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.shiro.data.entity.RoleInformation;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface RoleMapper extends BaseMapper<RoleInformation> {
-    List<String> getRolesByUserName(@Param("username") String username);
+    List<String> getRolesByUserName(String username);
 }
