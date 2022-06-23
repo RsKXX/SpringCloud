5f854002550cb7c9ec5857a0636aa8ce106f4dd2
RuanShaoKang <1584853990@qq.com>
2022-06-23 15:23:47 +0800

    rabbitmq init

---
+++ rabbitmq/src/main/java/com/rabbitmq/Listener/InfoListener.java
@@ -0,0 +1,15 @@
+package com.rabbitmq.Listener;
+
+import com.rabbitmq.constant.RabbitMqConstant;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InfoListener {
+    @RabbitHandler
+    @RabbitListener(queues = RabbitMqConstant.QUEUE_INFO)
+    public void data(String message){
+        System.out.println(message);
+    }
+}
---
+++ rabbitmq/src/main/java/com/rabbitmq/constant/RabbitMqConstant.java
@@ -0,0 +1,17 @@
+package com.rabbitmq.constant;
+
+public class RabbitMqConstant {
+//    public static final String EXCHANGE_DIRECT = "demo.exchange.direct";
+//    public static final String DEAD_LETTER_QUEUE = "demo.queue.dead-letter";
+
+    /**
+     * exchange
+     */
+    public static final String EXCHANGE_TOPIC = "canal.exchange";
+    /**
+     * 队列
+     */
+    public static final String QUEUE_INFO = "canal.queue";
+
+    public static final String ROUTING_KEY = "canal.routing.key";
+}
